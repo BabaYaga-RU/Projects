04de25638dc84b1bec1ec7e8126640b51d06fca6
𝓟aͥtriͣcͫ𝒌 <patrickajm@gmail.com>
2026-03-22 20:50:16 -0300

    quinto exercicio parcial

--- 2026_2/Estrutura_de_dados/exercise/primeira_entrega/Main.java
+++ 2026_2/Estrutura_de_dados/exercise/primeira_entrega/Main.java
@@ -153,7 +153,40 @@ public class Main {
         b) um vetor criado com os números de 1 até 100 ordenados em ordem decrescente
         */
         try{
+            Random random = new Random();
+            int[] vetor_um = new int[100];
+            int indice = 0;
+            while(true){
+                int valor_atual = random.nextInt(100);
+                boolean contem = false;
+                boolean final_vetor = true;
+                for (int i = 0; i < 100; i++){
+                    if (vetor_um[i] == valor_atual + 1){
+                        contem = true;
+                    }
+                    if (vetor_um[i] == 0){
+                        final_vetor = false;
+                    }
+                }
+                if (final_vetor == true){
+                    break;
+                }
+                if (contem == false){
+                    vetor_um[indice] = valor_atual + 1;
+                    indice ++;
+                }
+            }
+            System.out.println("Primeiro vetor: " + Arrays.toString(vetor_um));
+            int[] vetor_dois = new int[100];
+            indice = 0;
+            for (int i = 100; i > 0; i--){
+                vetor_dois[indice] = i;
+                indice ++;
+            }
+            System.out.println("Segundo vetor: " + Arrays.toString(vetor_dois));
 
+            
+            
         }catch(Exception e){}
     }
 }
