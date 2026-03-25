03a7d1d42089958bc7e6d1c77d6bbdf4d4f5a950
𝓟aͥtriͣcͫ𝒌 <patrickajm@gmail.com>
2026-03-24 22:12:10 -0300

    up

--- 2026_1/Simulacao_de_sistemas_computacionais/exercice/segunda_entrega/Sistema_de_Filas.py
+++ 2026_1/Simulacao_de_sistemas_computacionais/exercice/segunda_entrega/Sistema_de_Filas.py
@@ -1,6 +1,6 @@
 import random
 import math
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as matplotlib
 import numpy as numpy_np
 import pandas as pandas_pd
 
@@ -45,8 +45,48 @@ def calcular_estatistica(lista_numeros):
 
     return estatistica
 
+def gerar_dataframe(resultados_simulacao):
+    # Transformar em DataFrame (estilo Excel) e deixar na vertical usando T 
+    data_frame = pandas_pd.DataFrame(resultados_simulacao).T
 
+    # Dar nome a coluna dos valores
+    data_frame.columns = ['valor']
+    #Criar a linha sobre a configuracao da simulacao 
+    data_frame.loc['Configuracao'] = 'Taxa Chegada: 3, Taxa Atendimento: 5, Tempo: 1000'
 
+    return data_frame
 
+def comparador_matematico(resultados_simulacao):
+    dados_comparacao = {
+        'Metrica': [
+            'Tempo Medio no Sistema',
+            'Tempo Medio na Fila',
+            'Numero Medio no Sistema',
+            'Numero Medio na Fila'
+        ],
+        'Simulado': [
+            resultados_simulacao['tempo_medio_sistema_simulado'],
+            resultados_simulacao['tempo_medio_fila_simulado'],
+            resultados_simulacao['numero_medio_sistema_simulado'],
+            resultados_simulacao['numero_medio_fila_simulado']
+        ],
+        'Teorico': [
+            resultados_simulacao['tempo_medio_sistema_teorico'],
+            resultados_simulacao['tempo_medio_fila_teorico'],
+            resultados_simulacao['numero_medio_sistema_teorico'],
+            resultados_simulacao['numero_medio_fila_teorico']
+        ]
+    }
+    data_frame = pandas_pd.DataFrame(dados_comparacao)
 
-print(gerar_intervalos(10))
+    # Calcular diferenca 
+    data_frame['Diferenca'] =  data_frame['Simulado'] - data_frame['Teorico']
+    data_frame['Erro (%0)'] = data_frame['Simulado'] / data_frame['Teorico'] * 100
+
+    return data_frame
+
+def exportar(data_frame):
+    try:
+        data_frame.to_excel('dataframe.xlxs', index = False)
+    except Exception as e:
+        print('Erro ao exportar')
